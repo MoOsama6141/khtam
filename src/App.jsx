9747ff6f8552f289e5e842b5c03c7e2689c6e970
mostafa-osama <front-007@almusanid.com>
2026-06-10 15:45:31 +0300

    fixing gaps between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { GrLinkNext } from "react-icons/gr";
 
-
 function App({ goToSecondSection }) {
   return (
     <div className="w-screen h-[100vh] bg-home  relative flex justify-center items-end">
-      <div className=" flex flex-col justify-center items-center gap-4 bg- w-fit mx-auto  text-[#f8f5eecc] font-medium mb-8 p-2 rounded-lg">
-        <button className="py-4 px-8 rounded-4xl bg-[#0a0a0a80]/60 shadow-sm hover:bg-[#0a0a0a80]/80  flex gap-4 text-lg cursor-pointer justify-center items-center " onClick={goToSecondSection}>Enter menu <span><GrLinkNext />
-</span></button>
+      <div className=" flex flex-col justify-center items-center gap-5  w-fit mx-auto  text-[#f8f5eecc] font-medium mb-6 p-2 rounded-lg">
+        <button
+          className="py-4 px-8 rounded-4xl bg-[#0a0a0a80]/60 shadow-sm hover:bg-[#0a0a0a80]/80  flex gap-4 text-lg cursor-pointer justify-center items-center "
+          onClick={goToSecondSection}
+        >
+          Enter menu{" "}
+          <span>
+            <GrLinkNext />
+          </span>
+        </button>
         <span className="font-bold text-xl ">Made By Mo Osama</span>
       </div>
     </div>
